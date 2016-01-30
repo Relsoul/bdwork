f112a9c017046979d14876497af9cfd763a3bc9d
relsoul <admin@emufan.com>
2016-01-31 00:34:25 +0800

    ''

--- soul/soulChat/config/config.js
+++ soul/soulChat/config/config.js
@@ -3,18 +3,14 @@
  */
 
 
-var obj={
-    host:"soulchat.cn",
-    static:"public",//相当于__dirname
-    view:"./app/views",
-    upload:this.static+"/upload",
-    avatar:"http://"+this.host+"/"+this.upload+"/avatar",
+
+
+var obj=function(){
+    this.host='soulchat.cn';
+    this.static='public';
+    this.view="./app/views";
+    this.upload=this.static+"/upload";
+    this.avatar="http://"+this.host+"/"+this.upload+"/avatar";
 }
 
-module.exports={
-    host:obj.host,
-    static:obj.static,//相当于__dirname
-    view:obj.view,
-    upload:obj.upload,
-    avatar:obj.avatar
-}
+module.exports=obj
