16f54bcf0184fd3348a28654901ad83d96dc5b55
relsoul <admin@emufan.com>
2016-01-31 00:26:56 +0800

    ''

--- soul/soulChat/config/config.js
+++ soul/soulChat/config/config.js
@@ -1,10 +1,20 @@
 /**
  * Created by soul on 2016/1/19.
  */
-module.exports={
+
+
+var obj={
     host:"soulchat.cn",
     static:"public",//相当于__dirname
     view:"./app/views",
     upload:this.static+"/upload",
     avatar:"http://"+this.host+"/"+this.upload+"/avatar",
+}
+
+module.exports={
+    host:obj.host,
+    static:obj.static,//相当于__dirname
+    view:obj.view,
+    upload:obj.upload,
+    avatar:obj.avatar
 }
