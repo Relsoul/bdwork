ea315ec68d224f4127b1a77a93112dff02d21627
relsoul <admin@emufan.com>
2016-03-05 02:40:32 +0800

    update

--- soul/soulChat/public/js/soulChat.js
+++ soul/soulChat/public/js/soulChat.js
@@ -86,15 +86,16 @@ function chat($scope, $http, $cookies, socket, $stateParams, server, $rootScope,
 
 
 
-    //获取当前房间信息
-    $scope.room = server.getRoom($stateParams.roomId)
-
     server.joinRoom({
         userId: $rootScope.session_user["_id"],
         username: $rootScope.session_user["name"],
         roomId: $stateParams.roomId
     })
 
+    //获取当前房间信息
+    $scope.room = server.getRoom($stateParams.roomId)
+
+    
 
     setTimeout(function(){
         console.log("now room info",$scope.room)
